673046275f498ecbda3cef7d13109e2be90962d4
FUZZAITI <pedrofuzzaiti@gmail.com>
2025-09-08 16:42:38 -0300

    uma linha

--- get_next_line.c
+++ get_next_line.c
@@ -1,28 +1,65 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <fcntl.h> 
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepinhei <pepinhei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 11:33:25 by pepinhei          #+#    #+#             */
+/*   Updated: 2025/09/08 16:40:38 by pepinhei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h> // para o strcpy
+
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 15
+#endif
+
+char	*test(int fd);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+
+int	main(void)
 {
 	int		fd;
-	char	buf[50];
-	int		n;
+	char	*line;
 
 	fd = open("text.txt", O_RDONLY);
 	if (fd < 0)
-		return (1);
-
-	n = read(fd, buf, 35);
-	if (n < 0)
 	{
-		perror("read");
-		close(fd);
+		perror("open");
 		return (1);
 	}
-	buf[n] = '\0';
-	printf("%s", buf);
+	line = test(fd);
+	printf("%s", line);
+	free(line);
 	close(fd);
 	return (0);
+}
+
+char	*test(int fd)
+{
+	char	*buf;
+	int		n;
+	int		i;
+	char	*result;
+
+	i = 0;
+	n = 0;
+	buf = malloc(BUFFER_SIZE + 1);
+	result = malloc(BUFFER_SIZE + 1);
+	n = read(fd, buf, BUFFER_SIZE);
+	buf[n] = '\0';
+	while (buf[i] != '\0' && buf[i] != '\n')
+	{
+		i++;
+	}
+	strncpy(result, buf, i);
+	free(buf);
+	return (result);
 }
